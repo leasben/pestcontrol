502fb7ec072781ccf52e0307e623de48e0e56f4b
leasben <simbaleee123@gmail.com>
2025-12-02 19:32:37 +0200

    Done

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,13 @@ export default defineConfig({
         team: resolve(__dirname, 'our-team.html'),
         responsibleInvesting: resolve(__dirname, 'responsible-investing.html'),
         about: resolve(__dirname, 'about-us.html'),
-        funds: resolve(__dirname, 'funds.html'),
+        services: resolve(__dirname, 'services.html'),
         contact: resolve(__dirname, 'contact.html'),
+        geyserReplacement: resolve(__dirname, 'geyser-replacement.html'),
+        drainCleaning: resolve(__dirname, 'drain-cleaning.html'),
+        residentialPlumbing: resolve(__dirname, 'residential-plumbing.html'),
+        commercialPlumbing: resolve(__dirname, 'commercial-plumbing.html'),
+        remodellingRenovations: resolve(__dirname, 'remodelling-renovations.html'),
       },
       output: {
         dir: 'dist', // Output directory for the build
