cf070495f1ff70beeaaa31304286e95c46b69266
leasben <simbaleee123@gmail.com>
2026-04-17 13:10:57 +0200

    done

--- vite.config.js
+++ vite.config.js
@@ -9,16 +9,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'), 
-        team: resolve(__dirname, 'our-team.html'),
-        responsibleInvesting: resolve(__dirname, 'responsible-investing.html'),
-        about: resolve(__dirname, 'about-us.html'),
-        services: resolve(__dirname, 'services.html'),
-        contact: resolve(__dirname, 'contact.html'),
-        geyserReplacement: resolve(__dirname, 'geyser-replacement.html'),
-        drainCleaning: resolve(__dirname, 'drain-cleaning.html'),
-        residentialPlumbing: resolve(__dirname, 'residential-plumbing.html'),
-        commercialPlumbing: resolve(__dirname, 'commercial-plumbing.html'),
-        remodellingRenovations: resolve(__dirname, 'remodelling-renovations.html'),
       },
       output: {
         dir: 'dist', // Output directory for the build
